7fca869c885c6656714ec80918b0dc0b851d959d
James Barlow <james@triumphtech.io>
2020-06-15 12:03:19 -0400

    adding db parameters

--- config/database.php
+++ config/database.php
@@ -45,17 +45,15 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env('RDS_HOSTNAME', $_SERVER['RDS_HOSTNAME']),
+            'port' => env('RDS_PORT', $_SERVER['RDS_PORT']),
+            'database' => env('RDS_DBNAME', $_SERVER['RDS_DBNAME']),
+            'username' => env('RDS_USER', $_SERVER['RDS_USER']),
+            'password' => env('RDS_PASSWORD', $_SERVER['RDS_PASSWORD']),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
